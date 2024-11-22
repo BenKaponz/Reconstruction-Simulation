dd8b5ea08c08ae4c41abfe3c0bf818b807a69d2d
BenKaponz <benkapon0560@gmail.com>
2024-11-22 16:37:58 +0200

    g

--- Skeleton/src/main.cpp
+++ Skeleton/src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 Simulation* backup = nullptr;
 
 int main(int argc, char** argv){
-     cout << "koosdodasheli" << endl;
+     cout << "koosdodashfegerggergeli" << endl;
     if(argc!=2){
         cout << "usage: simulation <config_path>" << endl;
         return 0;
