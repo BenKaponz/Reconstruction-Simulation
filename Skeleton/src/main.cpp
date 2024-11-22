7aab50248c82e224f2e7b80429cf8ef07abd049f
Itay Shaul <itayshaul111@gmail.com>
2024-11-22 16:26:08 +0200

    important print

--- Skeleton/src/main.cpp
+++ Skeleton/src/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 Simulation* backup = nullptr;
 
 int main(int argc, char** argv){
+     cout << "koosdodasheli" << endl;
     if(argc!=2){
         cout << "usage: simulation <config_path>" << endl;
         return 0;
