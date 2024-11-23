5384ec9065562c9a339684a7a2f5633cf4cb7765
BenKaponz <benkapon0560@gmail.com>
2024-11-23 17:37:02 +0200

    דד

--- Skeleton/src/Facility.cpp
+++ Skeleton/src/Facility.cpp
@@ -50,3 +50,7 @@ FacilityCategory FacilityType::getCategory() const
 {
     return category;
 }
+
+Facility::Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score)
+{
+}
