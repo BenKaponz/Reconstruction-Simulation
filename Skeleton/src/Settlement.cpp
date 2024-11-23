615e684a473a482b272baaee73f413de96cf1ef1
BenKaponz <benkapon0560@gmail.com>
2024-11-23 16:34:48 +0200

    ד

--- Skeleton/src/Settlement.cpp
+++ Skeleton/src/Settlement.cpp
@@ -1,50 +1,44 @@
 #include "Settlement.h"
 #include <iostream>
+using std::string;
+using std::vector;
+
 
 // Constructor
 Settlement::Settlement(const string &name, SettlementType type)
     : name(name), type(type) {
-    std::cout << "Settlement Constructor Called for: " << name << std::endl;
 }
 
 // Copy Constructor
 Settlement::Settlement(const Settlement &other)
     : name(other.name), type(other.type) {
-    std::cout << "Settlement Copy Constructor Called for: " << name << std::endl;
 }
 
 // Copy Assignment Operator
 Settlement &Settlement::operator=(const Settlement &other) {
     if (this == &other) {
         return *this; // Handle self-assignment
-    }
-    // The `name` is const and cannot be reassigned.
-    // The type is the only assignable field.
-    const_cast<SettlementType&>(type) = other.type;
-
-    std::cout << "Settlement Copy Assignment Operator Called for: " << name << std::endl;
+    }   
+    // לבדוק מה קורה עם השם 
+    type = other.type;
     return *this;
 }
 
 // Destructor
 Settlement::~Settlement() {
-    std::cout << "Settlement Destructor Called for: " << name << std::endl;
 }
 
 
 
-SettlementType Settlement::getType() const
-{
+SettlementType Settlement::getType() const{
     return SettlementType();
 }
 
-const string Settlement::toString() const
-{
-    return string();
+const string Settlement::toString() const{
+    return "Settlement: " + name;
 }
 
-const string &Settlement::getName() const
-{
-    // TODO: insert return statement here
+const string &Settlement::getName() const{
+    return name;
 }
 
