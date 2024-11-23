9d795aa8893f2c392c1464c5ec1f32fc1be569f0
BenKaponz <benkapon0560@gmail.com>
2024-11-23 16:00:05 +0200

    s

---
+++ Skeleton/src/Settlement.cpp
@@ -0,0 +1,21 @@
+#include "Settlement.h"
+
+Settlement::Settlement(const string &name, SettlementType type)
+{
+}
+
+SettlementType Settlement::getType() const
+{
+    return SettlementType();
+}
+
+const string Settlement::toString() const
+{
+    return string();
+}
+
+const string &Settlement::getName() const
+{
+    // TODO: insert return statement here
+}
+
